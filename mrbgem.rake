41b198b775b4d715834b8009bab369600556fd3a
tos-miyake <tosihiro0917@gmail.com>
2019-07-21 16:54:17 +0900

    fix

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,4 +1,6 @@
 MRuby::Gem::Specification.new('mruby-loadavg') do |spec|
   spec.license = 'MIT'
   spec.authors = 'tos-miyake'
+
+  spec.add_dependency 'mruby-io'
 end
--- mrblib/mrb_loadavg.rb
+++ mrblib/mrb_loadavg.rb
@@ -1,5 +1,18 @@
 class Loadavg
-  def bye
-    self.hello + " bye"
+
+  def loadavg_1
+    read_loadavg.split[0].to_f
+  end
+
+  def loadavg_5
+    read_loadavg.split[1].to_f
+  end
+
+  def loadavg_15
+    read_loadavg.split[3].to_f
+  end
+
+  private def read_loadavg
+    IO.read('/proc/loadavg')
   end
 end
